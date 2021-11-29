bdcb0825ba871190f48159b3afd152136f8abda5
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-11-29 08:34:18 +0530

    Add files via upload

---
+++ variables03.java
@@ -0,0 +1,15 @@
+public class variables03 {
+    public static void main(String[] args){
+        //variables
+        int a = 100;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+        int diff = a - b; 
+        System.out.println(diff);
+        int mul = a * b;
+        System.out.println(mul);
+        int div = a/b;
+        System.out.println(div);
+    }
+}
