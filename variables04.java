6587b6eac6bf910ad488533b5bce3e0cfc88d0bc
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-11-29 08:34:36 +0530

    Add files via upload

---
+++ variables04.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class variables04 {
+    public static void main(String[] args){
+        //INPUT
+        System.out.print("Input name : ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //nextInt()
+        //nextFloat()
+        System.out.println(name);
+    }
+}
