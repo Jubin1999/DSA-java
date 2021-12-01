e081a2cf551cea84cf4f246fc6138da2e20766ca
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-01 08:49:15 +0530

    Add files via upload

---
+++ loops06.java
@@ -0,0 +1,10 @@
+public class loops06 {
+    public static void main(String args[]){
+        //while loops
+        int i = 0;
+        while(i<11){
+            System.out.print(i+" ");
+            i = i + 1;
+        }
+    }
+}
---
+++ loops07.java
@@ -0,0 +1,10 @@
+public class loops07 {
+    public static void main(String args[]){
+        //do while loops
+        int i=0;
+        do{
+            System.out.print(i+" ");
+            i = i + 1;
+        }while(i<11);
+    }
+}
