a45744f7ab1c95b1480ad3de5a19572fc1801890
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-01 08:49:36 +0530

    Add files via upload

---
+++ loops08.java
@@ -0,0 +1,13 @@
+public class loops08 {
+    public static void main(String args[]){
+
+        int i = 12;
+        while(i < 11){
+            System.out.println("Apna college");
+        }
+
+        do{
+            System.out.println("Apna college");
+        }while(i < 11);
+    }
+}
