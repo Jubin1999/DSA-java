126792e75f9a1df1e5275fb232f7598c08edfdf8
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-01 08:47:35 +0530

    Add files via upload

---
+++ loops02.java
@@ -0,0 +1,7 @@
+public class loops02 {
+    public static void main(String args[]){
+        for(int counter = 0; counter < 10; counter = counter + 1){
+            System.out.println("Hello World");
+        }
+    }
+}
