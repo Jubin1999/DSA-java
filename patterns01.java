b51d7ea7e0bafef59f1b6222a4503b0102b70331
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-04 22:07:52 +0530

    Add files via upload

---
+++ patterns01.java
@@ -0,0 +1,15 @@
+public class patterns01 {
+    public static void main(String ages[]){
+        int n = 4;
+        int m = 5;
+
+        //outer loop
+        for(int i = 1; i<=n; i++){
+            //inner loop
+            for(int j = 1; j<=m; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
