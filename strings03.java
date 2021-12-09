b9a96c619dd4adc1c6690eebfe5581bb381bfc6d
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-09 13:38:22 +0530

    Add files via upload

---
+++ strings03.java
@@ -0,0 +1,12 @@
+public class strings03 {
+    public static void main(String args[]){
+        String name1 = "Tony";
+        String name2 = "Tonya";
+
+        if(name1.compareTo(name2) == 0){
+            System.out.println("String are equal");
+        }else{
+            System.out.println("String are not equal");
+        }
+    }
+}
