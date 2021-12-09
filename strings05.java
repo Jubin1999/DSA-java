dc858c035ee5e686fe5dc56076030163cdcc0c01
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-09 13:38:57 +0530

    Add files via upload

---
+++ strings05.java
@@ -0,0 +1,7 @@
+public class strings05 {
+    public static void main(String args[]){
+        String sentence = "TonyStark";
+        String name = sentence.substring(0,4);
+        System.out.println(name);
+    }
+}
