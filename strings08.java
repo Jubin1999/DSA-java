d25b3c2a161cb5a49a901acf741bc0d41e630ec8
Jubin Deori <89245775+Jubin1999@users.noreply.github.com>
2021-12-09 13:39:55 +0530

    Add files via upload

---
+++ strings08.java
@@ -0,0 +1,11 @@
+public class strings08 {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("H");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+}
